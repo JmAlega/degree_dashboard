be7aea5aed9060a7ed66babc5364c93e8aa3f320
Sreevaishnav Deevi <vineteya@gmail.com>
2020-10-21 09:17:43 -0500

    Added ability to send emails to user

--- API/app/routes/index.js
+++ API/app/routes/index.js
@@ -1,5 +1,10 @@
 const courseRoutes = require('./course_routes.js');
+const authentication = require('./authentication.js')
 
 module.exports = function(app, db) {
   courseRoutes(app, db);
+}
+
+module.exports = function(app, nodemailer) {
+  authentication(app, nodemailer);
 }
--- API/server.js
+++ API/server.js
@@ -1,21 +1,29 @@
-const express     = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser  = require('body-parser');
-const db          = require('./config/db');
+const express         = require('express');
+const MongoClient     = require('mongodb').MongoClient;
+const bodyParser      = require('body-parser');
+const db              = require('./config/db');
+const app             = express();
 
-const app         = express();
 app.use(bodyParser.json());
 
 const port = 8000;
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+// Connect to Mongo Client
+// Import DB Routes
 const client = new MongoClient(db.url, { useNewUrlParser: true, useUnifiedTopology: true })
 client.connect(err => {
   if (err) return console.log(err)
   const db = client.db("Subjects")
   require('./app/routes')(app, db);
-  app.listen(port, () => {
-    console.log("We are live on " + port);
-  });
-})
+}); 
+
+// Import Authentication Routes 
+const nodemailer = require('nodemailer')
+require('./app/routes')(app, nodemailer);
+
+// Listen to port
+app.listen(port, () => {
+  console.log("We are live on " + port);
+});
