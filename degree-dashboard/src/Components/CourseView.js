03c34b08ac935e3bedb123987433547a535c2b43
Austin <auveltr98@gmail.com>
2020-11-02 21:48:55 -0600

    Restyled components

---
+++ degree-dashboard/src/Components/CourseView.js
@@ -0,0 +1,13 @@
+// THIS COMPONENT IS JUST A PLACEHOLDER! PUT THE ACTUAL COURSEVIEW COMPONENT HERE
+import React from 'react';
+import Header from'./Header';
+const CourseView = () => {
+  return (
+    <div className="App">
+      <Header className="App-header"/>
+        COURSEVIEW
+    </div>
+  )
+}
+
+export default CourseView;
