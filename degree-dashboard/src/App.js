976f0f9f3c6af1e815bae0c1481f64272f38dc36
edsn52 <edsn52@mst.edu>
2020-11-06 16:46:23 -0600

    Added material design

--- degree-dashboard/src/App.js
+++ degree-dashboard/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+
 import SemesterPlan from './semesterPlan.js'
 
 function App() {
@@ -7,16 +7,32 @@ function App() {
   const schedule = [
         { 
 		  semester: 'Spring 2021',
-          title: ['FR ENG 1100', 'CS 1500', 'MATH 1214', 'ENGLISH 1120', 'GEO 1120'],
-		  description: ['Study And Careers In Engineering and Computing', 'Computational Problem Solving', 'Calculus For Engineers I', 'Exposition And Argumentation', 'Evolution Of The Earth']
-		  
+          courses: [ 
+        {'title': 'FR ENG 1100', 'description': 'Study And Careers in Engineering and Computing'},
+			{'title': 'CS 1500', 'description': 'Computational Problem Solving'},
+			{'title': 'MATH 1214', 'description': 'Calculus For Engineers I'},
+			{'title': 'ENGLISH 1120', 'description': 'Exposition And Argumentation'},
+			{'title': 'GEO 1120', 'description': 'Evolution Of The Earth'},
+		  ]
         },
         { 
           semester: 'Fall 2021',
-          title: ['CS 1200', 'CS 1570', 'CS 1580', 'MATH 1215'],
-		  description: ['Discrete Mathematics for Computer Science', 'Introduction To C++ Programming', 'Introduction To Programming Laboratory ', 'Calculus For Engineers II']
+          courses: [ 
+        {'title': 'CS 1200', 'description': 'Discrete Mathematics for Computer Science'},
+			{'title': 'CS 1570', 'description': 'Introduction To C++ Programming'},
+			{'title': 'CS 1580', 'description': 'Introduction To Programming Laboratory'},
+			{'title': 'MATH 1215', 'description': 'Calculus For Engineers II'},
+		  ]
+        },
+		{ 
+          semester: 'Spring 2022',
+          courses: [ ]
+        },
+		{ 
+          semester: 'Fall 2022',
+          courses: [ ]
         }]
-		
+		//change to course dictionary
 
   return (
     <div className="App">
--- degree-dashboard/src/CurrentClasses.js
+++ degree-dashboard/src/CurrentClasses.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import './CurrentClasses.css';
 
-function CurrentClasses ({title, semester, description}) {
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import { makeStyles } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Box from '@material-ui/core/Box';
+
+
+  const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 325,
+	height: 150,
+  },
+
+}));
+
+function CurrentClasses ({semester, courses}) {
  
   var buttonDescriptions = new Array(5);
   var i;
-  for(i = 0; i < title.length; i++)
+
+  if(courses.length < 5)
   {
-	  buttonDescriptions[i] = <div>
-								{title[i]}
-								<br />
-								<br />
-								{description[i]}
-								</div>
-  }
-  if(title.length < 5)
-  {
-	for(i = title.length; i < 5; i++)
+	for(i = courses.length; i < 5; i++)
 		buttonDescriptions[i] = <div>
 								Choose Courses
 								<br />
@@ -24,23 +37,35 @@ function CurrentClasses ({title, semester, description}) {
 								Click to view all options
 								</div>
   }
+  
+  const classes = useStyles();
+
   return (
-    <div className="classes">
-      <section className="buttons">
-			<button className="courseButton">{buttonDescriptions[0]}</button>
-      </section>
-	  <section className="buttons">
-			<button className="courseButton">{buttonDescriptions[1]}</button>
-      </section>
-	  <section className="buttons">
-			<button className="courseButton">{buttonDescriptions[2]}</button>
-      </section>
-	  <section className="buttons">
-			<button className="courseButton">{buttonDescriptions[3]}</button>
-      </section>
-	  <section className="buttons">
-			<button className="courseButton">{buttonDescriptions[4]}</button>
-      </section>
+    <div style={{
+		display: 'flex',
+		alignItems: 'center'
+	}}>
+
+
+	{courses.map(course => 
+	<Box paddingRight={2} paddingBottom={2}>
+	<Card className={classes.root}>
+      <CardHeader
+        action={
+          <IconButton aria-label="settings">
+            <CloseIcon />
+          </IconButton>
+        }
+        title={course.title}
+      />
+	  <CardContent>
+        <Typography variant="body2" color="textSecondary">
+		{course.description}
+        </Typography>
+      </CardContent>
+	</Card>
+	</Box>)
+	}
 
     </div>
   )
